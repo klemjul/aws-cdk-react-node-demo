43ca4b6d8ce520eaeae020a4b557c2f59bfef32a
klemjul <barberisclementpro@gmail.com>
2024-01-24 22:44:04 +0100

    Initial commit

---
+++ test/aws-cdk-react-node-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkReactNodeDemo from '../lib/aws-cdk-react-node-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-react-node-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkReactNodeDemo.AwsCdkReactNodeDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
